2f460636e991359bbcab3cbafa7652aab978dc12
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:07:25 +0100

    fini

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 3 Exercice 4</title>
+  </head>
+  <body>
+    <?php
+    $number=1;
+    $result = $number / 2;
+    while(($number + $result) <= 10){
+      $result = $number / 2;
+      $number = $number + $result;
+      echo $number.'<br />';
+    }
+     ?>
+
+  </body>
+</html>
